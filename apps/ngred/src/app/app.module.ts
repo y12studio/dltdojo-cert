4242ca1a96c4ce210ea6b08909006b1a40b33ae1
y12studio <y12studio@gmail.com>
2017-04-29 00:03:37 +0800

    apps/ngred add angular material2 components

--- apps/ngred/src/app/app.module.ts
+++ apps/ngred/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BitcoreComponent } from './bitcore/bitcore.component';
@@ -13,6 +15,8 @@ import { BitcoreComponent } from './bitcore/bitcore.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule,
     FormsModule,
     HttpModule
   ],
